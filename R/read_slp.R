c89ffb5b5728af6fdb0edc4048eb591a2b1363cb
Jarad Niemi <niemi@iastate.edu>
2020-01-24 15:28:42 -0800

    Add read_slp() function

--- R/read_slp.R
+++ R/read_slp.R
@@ -1,10 +1,59 @@
-#' Read a WEPP soil file
+#' Read a WEPP slope file
 #'
-#' Reads a Water Erosion Prediction Project (WEPP) soil (*.sol) file.
-#' This file contains soil information.
+#' Reads a Water Erosion Prediction Project (WEPP) slope (*.slp) file.
+#' This file contains slope information assumed to be in nondimensional distances.
 #'
 #' @param file A path to the file.
+#' @return A \code{data.frame} with attributes version control number,
+#'   aspect of the profile (degrees from North) (azm), and
+#'   representative profile width(m) fwidth, and the
+#'   following columns:
+#'   \describe{
+#'     \item{n}{overland flow element (OFE) #}
+#'     \item{p}{proportion of the total distance for this OFE}
+#'     \item{slope}{vertical distance lost (m) per meter traveled}
+#'     \item{distance}{proportion (p) times total distance (m)}
+#'   }
+#' @export
 #'
-read_sol <- function(file) {
-  warning("This function is not yet implemented.")
+read_slp <- function(file) {
+
+  d = readLines(file)
+
+  version = d[1]
+
+  i = 2
+  while (substr(d[i], 1, 1) == "#")
+    i = i+1
+  nelem = as.numeric(d[i]) # number of overland flow elements (OFEs)
+
+  i = i+1
+  tmp = as.numeric(strsplit(d[i], split = " ")[[1]])
+  azm    = tmp[1] # aspect of the profile (degrees from North)
+  fwidth = tmp[2] # representative profile width (m)
+
+  OFEs = list()
+  for (n in 1:nelem) {
+    i = i+1
+    tmp = as.numeric(strsplit(d[i], split = " ")[[1]])
+    nsplts = tmp[1] # number of splits
+    slplen = tmp[2] # length of OFE (m)
+
+    i = i+1
+    tmp = as.numeric(gsub(",","",strsplit(d[i], split = c(" ",", "))[[1]][-1]))
+    p     = tmp[seq(1,2*nsplts,by=2)]
+    slope = tmp[seq(2,2*nsplts,by=2)]
+
+    OFEs[[n]] = data.frame(n = n, p = p, slope = slope, distance = p*slplen)
+  }
+  OFEs = do.call(rbind, OFEs)
+  if (any(OFEs$p>1))
+    stop("OFEs are not in nondimensional distances")
+
+  class(OFEs) <- append(class(OFEs), "slp")
+  attr(OFEs, "version") <- version
+  attr(OFEs, "azm")     <- azm
+  attr(OFEs, "fwidth")  <- fwidth
+
+  return(OFEs)
 }
