f788968f166817fa5be93f6760834efd9e2e1973
Jarad Niemi <niemi@iastate.edu>
2020-01-28 13:52:28 -0800

    Add write_wb()
    also read_wb() but I don't think we need this

--- R/read_wb.R
+++ R/read_wb.R
@@ -1,10 +1,25 @@
 #' Read a WEPP wb file
 #'
-#' Reads a Water Erosion Prediction Project (WEPP) wb (*.wb) file.
-#' This file contains water balance (?) information.
+#' Reads a Water Erosion Prediction Project (WEPP) soil water balance (*.wb) file.
+#' This file contains soil water balance information.
 #'
 #' @param file A path to the file.
+#' @return A data.frame with the columns:
+#' \describe{
+#'   \item{ofe}{numeric, overland flow element}
+#'   \item{jday}{numeric (1-366), julian day}
+#'   \item{year}{numeric, YYYY}
+#'   \item{precip}{numeric, amount of precipitation}
+#'   \item{runoff}{numeric, amount of runoff}
+#'   \item{sw}{numeric}
+#'   \item{sw1}{numeric}
+#'   \item{sw2}{numeric}
+#'   \item{ep}{numeric}
+#'   \item{es}{numeric}
+#'   \item{er}{numeric}
+#' }
+#' @export
 #'
 read_wb <- function(file) {
-  warning("This function is not yet implemented.")
+  read.table(file, header = TRUE)
 }
--- R/write_wb.R
+++ R/write_wb.R
@@ -1,11 +1,12 @@
 #' Write a WEPP wb file
 #'
-#' Writes a Water Erosion Prediction Project (WEPP) wb (*.wb) file.
-#' This file contains water balance information.
+#' Writes a Water Erosion Prediction Project (WEPP) soil water balance (*.wb) file.
+#' This file contains soil water balance information.
 #'
 #' @param wb A \code{wb} object.
 #' @param path Path to write to.
 #'
 write_wb <- function(wb, path) {
-  warning("This function is not yet implemented.")
+  write.table(wb, file = path,
+              col.names = FALSE)
 }
