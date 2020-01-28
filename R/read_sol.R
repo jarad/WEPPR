bc9032c49497ea9c7f5a404e1821bf2109b55de0
Jarad Niemi <niemi@iastate.edu>
2020-01-28 13:34:59 -0800

    Add write_sol() and read_sol()

--- R/read_sol.R
+++ R/read_sol.R
@@ -4,7 +4,59 @@
 #' This file contains soil information.
 #'
 #' @param file A path to the file.
+#' @export
 #'
 read_sol <- function(file) {
-  warning("This function is not yet implemented.")
+  d = readLines(file)
+
+  tmp = as.numeric(strsplit(d[3], split = " ")[[1]])
+  ntemp = tmp[1]
+  ksflag = tmp[2]
+
+  OFEs = list()
+  layers = list()
+  conductivity = list()
+  i = 4
+  for (n in 1:ntemp) {
+    # line 1 for this soil type
+    tmp = strsplit(d[i], split = " ")[[1]]
+
+    nsl = as.integer(tmp[3])
+    OFEs[[n]] = data.frame(layer  = n,
+                           slid   = tmp[1],
+                           texid  = tmp[2],
+                           # nsl    = nsl,
+                           salb   = as.numeric(tmp[4]),
+                           sat    = as.numeric(tmp[5]),
+                           ki     = as.numeric(tmp[6]),
+                           kr     = as.numeric(tmp[7]),
+                           shcrit = as.numeric(tmp[8]),
+                           avke   = as.numeric(tmp[9]),
+                           stringsAsFactors = FALSE)
+
+    # following lines for this soil type
+    layers[[n]] = read.table(textConnection(d[i + c(1:nsl)]),
+                             col.names = c("solthk","sand","clay","orgmat","cec","rfg"))
+    layers[[n]]$layer = n
+
+    # soil conductivity lines
+    conductivity[[n]] = read.table(textConnection(d[i+nsl+1]),
+                              col.names = c("Ke","flag_Kb_Ke", "unknown"))
+    conductivity[[n]]$layer = n
+
+    i = i + 2 + nsl
+  }
+  OFEs   = do.call(rbind, OFEs)
+  layers = do.call(rbind, layers)
+  conductivity = do.call(rbind, conductivity)
+
+  soil = merge(OFEs, layers, by = "layer", all = TRUE)
+  soil = merge(soil, conductivity, by = "layer", all = TRUE)
+
+  class(soil) <- append(class(soil), "sol")
+  attr(soil, "datver") <- d[1]
+  attr(soil, "solcom") <- d[2]
+  attr(soil, "ksflag") <- ksflag
+
+  return(soil)
 }
--- R/write_sol.R
+++ R/write_sol.R
@@ -5,7 +5,53 @@
 #'
 #' @param sol A \code{sol} object.
 #' @param path Path to write to.
+#' @export
 #'
 write_sol <- function(sol, path) {
-  warning("This function is not yet implemented.")
+  write(attr(sol, "datver"), file = path)
+  write(attr(sol, "solcom"), file = path, append = TRUE)
+
+  ntemp <- length(unique(sol$layer))
+  write(paste(ntemp, attr(sol, "ksflag"), collapse = " "),
+        file = path, append = TRUE)
+
+  for (n in 1:ntemp) {
+    s = sol[sol$layer == n,]
+
+    nsl = nrow(s)
+    line1 = unique(s[,c("slid","texid","salb","sat","ki","kr","shcrit","avke")])
+    stopifnot(nrow(line1)==1)
+
+    write(paste(line1$slid,
+                line1$texid,
+                nsl,
+                format(line1$salb,   nsmall = 6),
+                format(line1$sat,    nsmall = 6),
+                format(line1$ki,     nsmall = 6),
+                format(line1$kr,     nsmall = 6),
+                format(line1$shcrit, nsmall = 6),
+                format(line1$avke,   nsmall = 6),
+                sep = " "),
+          file = path, append = TRUE)
+
+    for (r in 1:nsl) {
+      write(paste("  ", # Seems like this should be a tab
+                  format(s$solthk[r], nsmall = 0),
+                  format(s$sand[r],   nsmall = 1),
+                  format(s$clay[r],   nsmall = 1),
+                  format(s$orgmat[r], nsmall = 1),
+                  format(s$cec[r],    nsmall = 1),
+                  format(s$rfg[r],    nsmall = 1)),
+            file = path, append = TRUE)
+    }
+
+    line1 = unique(s[,c("Ke","flag_Kb_Ke","unknown")])
+    stopifnot(nrow(line1)==1)
+
+    write(paste(format(line1$Ke,         nsmall = 0),
+                format(line1$flag_Kb_Ke, nsmall = 6),
+                format(line1$unknown,    nsmall = 0),
+                sep = " "),
+          file = path, append = TRUE)
+  }
 }
