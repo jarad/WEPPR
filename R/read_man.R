ad3d0693ab096cb12deec65c52166d3a8dec5771
Gulzina Kuttubekova <gkuttubekova@Gulzinas-MacBook-Pro.local>
2021-02-21 22:13:14 -0800

    Modified read_man.R function (documentation is NOT finalized yet)

--- R/read_man.R
+++ R/read_man.R
@@ -4,7 +4,41 @@
 #' This file contains plant/management information.
 #'
 #' @param file A path to the file.
+#' @return A \code{man} \code{list} with management set-up
+#' !!! needs more comments here !!!
+#'   following elements:
+#'   \describe{
+#'     \item{manver}{}
+#'     \item{iofe}{}
+#'     \item{inyr}{}
+#'     \item{ncrop}{}
+#'     \item{crops}{}
+#'     \item{nop}{}
+#'     \item{operations}{}
+#'     \item{nini}{}
+#'     \item{ini}{}
+#'     \item{nsurf}{}
+#'     \item{surfeffects}{}
+#'     \item{ncnt}{}
+#'     \item{ndrain}{}
+#'     \item{nmscen}{}
+#'     \item{scens}{}
+#'     \item{mantitle}{}
+#'     \item{mandesc}{}
+#'     \item{nwsofe}{}
+#'     \item{inindx}{}
+#'     \item{nrots}{}
+#'     \item{nyears}{}
+#'     \item{rotations}{}
+#'   }
+#' @export
 #'
 read_man <- function(file) {
-  warning("This function is not yet implemented.")
+  require("reticulate")
+
+  py_function = py_run_file("./R/read_man.py", local = TRUE)$read_man
+
+  res = py_function(file)
+
+  return(res)
 }
