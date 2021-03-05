401c0a548de2412d5c813fe2ed21b15b6306013d
Gulzina Kuttubekova <gkuttubekova@Gulzinas-MacBook-Pro.local>
2021-03-05 08:26:51 -0800

    Added read_yld.R (review required)

--- R/read_yld.R
+++ R/read_yld.R
@@ -4,7 +4,64 @@
 #' This file contains yield information.
 #'
 #' @param file A path to the file.
+#' @export
 #'
 read_yld <- function(file) {
-  warning("This function is not yet implemented.")
+  d <- readLines(file)
+
+  yields= c()
+  crops = c()
+
+  blank_line = 0
+  line = 7
+
+
+  while (blank_line < 1) {
+    tmp = strsplit(d[line], split = ' ')[[1]]
+
+    if (length(tmp) >= 4) {
+      crop_id = as.numeric(tmp[6])
+      crop_type = tmp[8]
+
+      crops = rbind(crops, c(crop_id, crop_type))
+    } else {
+      blank_line = blank_line + 1
+    }
+    line = line + 1
+  }
+
+  # from the previous loop
+  line = line + 2
+
+  while (blank_line < 2) {
+    tmp = strsplit(d[line], split = ' ')[[1]]
+
+    # assure that NOT a blank line
+    if (length(tmp) >= 12) {
+      blank_line = 0
+
+      crop_type = as.numeric(tmp[6])
+      date = as.numeric(tmp[9])
+      OFE = as.numeric(tmp[13])
+      yield = as.numeric(tmp[18])
+      unit = tmp[19]
+      year = as.numeric(tmp[21])
+
+      row = c(crop_type, date, OFE, yield, unit, year)
+      yields = rbind(yields, row)
+    } else {
+      blank_line = blank_line + 1
+    }
+    line = line + 1
+  }
+
+  crops = data.frame(crops, row.names = NULL)
+  colnames(crops) = c('id', 'name')
+
+  yields = data.frame(yields, row.names = NULL)
+  colnames(yields) = c('crop_type', 'date', 'OFE', 'yield', 'unit', 'year')
+
+  return(list(crops, yields))
+
+  # i'm ignoring here the average yield harvest here!!!!
 }
