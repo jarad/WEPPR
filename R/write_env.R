27baa36cc059957ccfa11e7c1a2170f1404942df
Jarad Niemi <niemi@iastate.edu>
2021-04-02 10:08:50 -0500

    Update documentation to mention units
    related to #8

--- R/write_env.R
+++ R/write_env.R
@@ -2,7 +2,14 @@
 #'
 #' Writes a Water Erosion Prediction Project (WEPP) environment (*.env) file.
 #'
-#' @param env A \code{env} object.
+#' The env object that is written must have a 'units' attribute. This is read
+#' from the env file when using the \code{\link{read_env}} function, but users
+#' need to be careful if they are modifying this env object that they maintain
+#' the units or modify the 'units' attribute appropriately. At this point, we
+#' have no idea how WEPP deals with units, i.e. whether it reads the units from
+#' the 3rd line in an env file or not.
+#'
+#' @param env A \code{env} object with 'units' attribute.
 #' @param path Path to write to.
 #' @export
 #'
