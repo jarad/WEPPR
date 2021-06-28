b2455433aa2a91e2e1acd59f4ce94358332ad7f8
Jarad Niemi <niemi@iastate.edu>
2021-06-28 08:24:12 -0500

    Add wepp_run_questions

--- data-raw/run_variables.R
+++ data-raw/run_variables.R
@@ -3,4 +3,8 @@ library("dplyr")
 run_variables <- readr::read_csv("run/run_variables.csv") %>%
   as.data.frame
 
+wepp_run_questions <- readr::read_csv("run/wepp_run_questions.csv") %>%
+  as.data.frame
+
 usethis::use_data(run_variables, overwrite = TRUE)
+usethis::use_data(wepp_run_questions, overwrite = TRUE)
